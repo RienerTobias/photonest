559bde0ae9f58bdde9e22d265cb8c314ed8cdcc9
Tobias Riener <tobias.riener@htlwy.at>
2025-03-25 14:02:50 +0100

    updated tailwindcss to v4

--- photonest_server/theme/static_src/tailwind.config.js
+++ photonest_server/theme/static_src/tailwind.config.js
@@ -41,17 +41,4 @@ module.exports = {
          */
         // '../../**/*.py'
     ],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-        /**
-         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-         * for forms. If you don't like it or have own styling for forms,
-         * comment the line below to disable '@tailwindcss/forms'.
-         */
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/aspect-ratio'),
-    ],
 }
