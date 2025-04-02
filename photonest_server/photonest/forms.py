dfd0cd8d3e0b76e7556ba1a05e18c6404f722535
Tobias Riener <tobias.riener@htlwy.at>
2025-04-02 20:37:52 +0200

    fixed multiple file upload and changed function

--- photonest_server/photonest/forms.py
+++ photonest_server/photonest/forms.py
@@ -2,37 +2,27 @@ from django import forms
 from django.core.validators import FileExtensionValidator
 from .models import Post, Media, SchoolClass
 
-class MediaForm(forms.ModelForm):
-    class Meta:
-        model = Media
-        fields = ['media_file', 'media_type', 'order']
-        widgets = {
-            'order': forms.NumberInput(attrs={'min': 0, 'max': 4})
-        }
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
 
-    def clean_media_file(self):
-        file = self.cleaned_data.get('media_file')
-        if file:
-            valid_extensions = ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'mov']
-            ext = file.name.split('.')[-1].lower()
-            if ext not in valid_extensions:
-                raise forms.ValidationError("Nur Bild- oder Video-Dateien sind erlaubt")
-        return file
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            return [single_file_clean(d, initial) for d in data]
+        return [single_file_clean(data, initial)]
 
 class PostForm(forms.ModelForm):
+    media_files = MultipleFileField(
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'mp4', 'mov'])],
+        required=False,
+        label='Medien (Mehrfachauswahl möglich)'
+    )
+
     class Meta:
         model = Post
         fields = ['school_class', 'description']
-        
-    def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)
-        super().__init__(*args, **kwargs)
-        self.fields['school_class'].queryset = SchoolClass.objects.all()
-
-    def save(self, commit=True):
-        post = super().save(commit=False)
-        if self.user:
-            post.user = self.user
-        if commit:
-            post.save()
-        return post
--- photonest_server/photonest/views.py
+++ photonest_server/photonest/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.forms import formset_factory
-from .forms import PostForm, MediaForm
-from .models import Post
 from django.views.decorators.http import require_POST
-from django.shortcuts import redirect, get_object_or_404
-
+from .forms import PostForm
+from .models import Post, Media
+import magic
 # Create your views here.
 @login_required
 def home(request):
@@ -14,30 +13,35 @@ def home(request):
 @login_required
 def gallery(request):
     if request.method == 'POST':
-        return create_post(request)
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            
+            # Medien verarbeiten
+            for file in request.FILES.getlist('media_files'):
+                mime = magic.Magic(mime=True)
+                content_type = mime.from_buffer(file.read(1024))
+                file.seek(0)
+                
+                media_type = 'photo' if content_type.startswith('image/') else 'video'
+                
+                media = Media.objects.create(
+                    media_file=file,
+                    media_type='photo' if content_type.startswith('image/') else 'video'
+                )
+                post.media_files.add(media) 
+            
+            return redirect('gallery')
     else:
-        post_form, media_formset = create_post(request)
-        return render(request, 'photonest/sites/gallery.html', {"posts": Post.objects.all(), "post_form": post_form, "media_formset": media_formset})
-
-@login_required
-def create_post(request):
-    MediaFormSet = formset_factory(MediaForm, extra=1, max_num=5)
+        form = PostForm()
     
-    if request.method == 'POST':
-        post_form = PostForm(request.POST, user=request.user)
-        media_formset = MediaFormSet(request.POST, request.FILES)
-        if post_form.is_valid() and media_formset.is_valid():
-            post = post_form.save()
-            for form in media_formset:
-                if form.cleaned_data.get('media_file'):
-                    media = form.save(commit=False)
-                    media.save()
-                    post.media_files.add(media)
-        return redirect('gallery')
-    else:
-        post_form = PostForm(user=request.user)
-        media_formset = MediaFormSet()
-        return post_form, media_formset,
+    posts = Post.objects.all().prefetch_related('media_files')
+    return render(request, 'photonest/sites/gallery.html', {
+        'posts': posts,
+        'form': form
+    })
 
 @login_required
 @require_POST
