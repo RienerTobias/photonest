185ee470ecf3a622766bbeb0e21309bfaaebfa50
Tobias Riener <tobias.riener@htlwy.at>
2025-04-06 10:49:08 +0200

    add django-filters

--- photonest_server/photonest_server/settings.py
+++ photonest_server/photonest_server/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django_browser_reload",
+    'django_filters',
     'tailwind',
     'theme',
     'widget_tweaks',
